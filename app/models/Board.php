e63811b1d7c410474c013f1460e9bc2ea3bb7686
Fntsrlike <fntsrlike@gmail.com>
2014-09-29 19:22:07 +0800

    Improve date filter of Board status, and create new scope "isEmpty()"!

--- app/models/Board.php
+++ app/models/Board.php
@@ -18,30 +18,46 @@ class Board extends Eloquent{
 
 	protected $fillable = array('type', 'code', 'description');
 
-	public function isUsing($from='', $end='') 
-    {
+	public function isUsing($from='', $end='')
+	{
 		if ( empty($from) or empty($end) ) {
-            $from = $end = date("Y-m-d") ;
-        }
-
+			$from = $end = date("Y-m-d") ;
+		}
 		$record = ApplyRecord::where('board_id', '=', $this->id)
-							->whereRaw(" (`post_from` <= $from AND `post_end` >= $from ) OR " .
-									   " (`post_from` <= $end AND `post_from` >= $end )   OR " .
-									   " (`post_from` >= $from AND `post_from` <= $end ) ")	
-							->first();
+		                    ->whereRaw( "(('$from' between `post_from` AND `post_end`) OR " .
+		                                " ('$end'  between `post_from` AND `post_end`) OR " .
+		                                " (`post_from` <= '$from' AND `post_end` >= '$end'))")
+		                    ->first();
 		return !is_null($record);
 	}
 
-    public function scopreIsUsing($query, $from='', $end='') 
-    {
-        if ( empty($from) or empty($end) ) {
-            $from = $end = date("Y-m-d") ;
-        }
+	public function scopeIsUsing($query, $from='', $end='')
+	{
+		if ( empty($from) or empty($end) ) {
+			$from = $end = date("Y-m-d") ;
+		}
 
-        return $query->where('board_id', '=', $this->id)
-                     ->whereRaw(" (`post_from` <= $from AND `post_end` >= $from ) OR " .
-                                " (`post_from` <= $end AND `post_from` >= $end )   OR " .
-                                " (`post_from` >= $from AND `post_from` <= $end ) ") ;
-                            
-    }
+		$records = ApplyRecord::whereRaw("(('$from' between `post_from` AND `post_end`) OR " .
+		                                 " ('$end'  between `post_from` AND `post_end`) OR " .
+		                                 " (`post_from` <= '$from' AND `post_end` >= '$end'))");
+		$using_boards = array_unique($records->lists('board_id'));
+
+		return $query->whereIn('id', $using_boards);
+
+	}
+
+	public function scopeIsEmpty($query, $from='', $end='')
+	{
+		if ( empty($from) or empty($end) ) {
+			$from = $end = date("Y-m-d");
+		}
+
+		$records = ApplyRecord::whereRaw("(('$from' between `post_from` AND `post_end`) OR " .
+		                                 " ('$end'  between `post_from` AND `post_end`) OR " .
+		                                 " (`post_from` <= '$from' AND `post_end` >= '$end'))");
+		$using_boards = array_unique($records->lists('board_id'));
+
+		return $query->whereNotIn('id', $using_boards);
+
+	}
 }
