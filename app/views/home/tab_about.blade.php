c8365e8c41ff461633acfaf5d075564750709196
Fntsrlike <fntsrlike@gmail.com>
2014-10-07 04:29:27 +0800

    Add content of about page

--- app/views/home/tab_about.blade.php
+++ app/views/home/tab_about.blade.php
@@ -1,7 +1,74 @@
 <div id="tab_about" class="container tab-pane">
   <div class="row">
-    <div class="col-md-12">
-        About
+  <div class="col-md-12">
+    <div class="container">
+      <section>
+        <h2>Rules</h2>
+        <div>
+          <h4>Common Rules</h4>
+          <ol>
+            <li>Any post should be checked and makerd from [MANAGER_UNIT] before posting. </li>
+            <li>You can't apply two large poster in the same time or continuously apply twice.</li>
+            <li>If you don't clear poster in last day, you will be baned 1 month.</li>
+          </ol>
+          <h4>Normal Poster</h4>
+          <ol>
+            <li>Application should apply before 2 days of you want to post.</li>
+            <li>
+              Each application can't post over 2 ~ 4 weeks.
+              <ul>
+                <li>2 Weeks: Internal Program - Orientation Party, Farewell Party</li>
+                <li>3 Weeks: External Program - Fairs, Camp</li>
+                <li>4 Weeks: Internclub Program - Joint program by two or more clubs.</li>
+              </ul>
+            </li>
+            <li>If you don't post in two days of from date of your application, that application will be cancel.</li>
+          </ol>
+          <h4>Stairs Poster</h4>
+          <ol>
+            <li>Same as rules of normal poster.</li>
+            <li>Each application can post once on every floor.</li>
+          </ol>
+          <h4>Large Poster</h4>
+          <ol>
+            <li>Application should apply before 2 weeks of you want to post.</li>
+            <li>Each application can't post over 6 days.</li>
+            <li>Poster length can't over 1st floor, width can't over 220 CM. </li>
+          </ol>
+        </div>
+      </section>
+      <section>
+        <h2>Managers</h2>
+        <p>
+          The boards and this system is managed by [MANAGER_UNIT], you can contact to us in following ways:
+          <ul>
+            <li>Email: poster@foo.org</li>
+            <li>Facebook: http://www.facebook.com/foo</li>
+            <li>Telphone: 01-2345-6789 (10:00 - 17:00, Mon. to Fri.)</li>
+          </ul>
+        </p>
+      </section>
+      <section>
+        <h2>System</h2>
+        <h4>Original Projects</h4>
+        <p>
+          This system is an open source project. There are some information about project in folowing:
+          <ul>
+            <li>CC: Attribution, NonCommercia, ShareAlike</li>
+            <li>Github: <a href="https://github.com/fntsrlike/boards_manager">https://github.com/fntsrlike/boards_manager</a></li>
+            <li>Author: <a href="http://about.me/fntsrlike">Ruoshi Ling</a> &lt;fntsrlike[at]gmail.com&gt;</li>
+          </ul>
+        </p>
+        <h4>Maintenance Unit</h4>
+        <p>
+          This system is maintained by IT Depart. of [MANAGER_UNIT], There are some information about us in folowing:
+          <ul>
+            <li>Github: <a href="https://github.com/fntsrlike/boards_manager">https://github.com/fntsrlike/boards_manager</a></li>
+          </ul>
+        </p>
+      </section>
+
     </div>
+  </div>
 </div>
 </div>
