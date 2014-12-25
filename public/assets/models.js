13bfc45ed62d46346f82cb38914dab872e4a27bc
Fntsrlike <fntsrlike@gmail.com>
2014-12-25 19:59:45 +0800

    Add put and delete models of javascript

--- public/assets/models.js
+++ public/assets/models.js
@@ -5,14 +5,36 @@ $( function(){
         $.get( url ).done( function( data ) {
             callback( data );
         });
-    }
+    };
 
     // POST method with serialize data
     models.post = function( url, selector, callback ) {
         $.post( url , $( selector ).serialize()).done( function( data ) {
             callback( data );
         });
-    }
+    };
+
+    // PUT method with serialize data
+    models.put = function( url, selector, callback ) {
+        $.ajax({
+            url: url,
+            type: 'PUT',
+            data: $( selector ).serialize()
+        }).success( function( data ) {
+            callback( data );
+        });
+    };
+
+    // DELETE method with serialize data
+    models.delete = function( url, selector, callback ) {
+        $.ajax({
+            url: url,
+            type: 'DELETE',
+            data: $( selector ).serialize()
+        }).success( function( data ) {
+            callback( data );
+        });
+    };
 
     // Login
     models.login = function( callback ) {
@@ -39,5 +61,24 @@ $( function(){
     models.readBoards = function( callback, args ) {
         if ( typeof( args ) === 'undefined' ) args = {};
         models.get( api.boards + '?' + $.param( args ), callback );
+    };
+
+    // Get boards data
+    models.readUsers = function( callback, args ) {
+        if ( typeof( args ) === 'undefined' ) args = {};
+        models.get( api.users + '?' + $.param( args ), callback );
+    };
+
+    models.updateBoards = function( callback ) {
+        id =
+        models.put( api.boards + '/' + $('#boardModifyId'), 'modal_boards_modify form', callback );
+    };
+
+    models.updateUsers = function( callback ) {
+        models.put( api.users + '/' + $('#userModifyId'), 'modal_users_modify form', callback );
+    };
+
+    models.updateRecords = function( callback ) {
+        models.put( api.records + '/' + $('#recordModifyId'), 'modal_records_modify form', callback );
     };
 });
